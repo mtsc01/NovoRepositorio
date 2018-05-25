25c748df03bbdca4ef3cf4b71ace9008eec43817
Mateus Tadeu <t.ro.nica@hotmail.com>
2018-05-25 16:47:55 -0300

    commit teste

---
+++ Baiano.java
@@ -0,0 +1,12 @@
+package novorepositorio;
+
+public class Baiano {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Teste - Welcome IFMG Sabará");
+
+	}
+
+}
